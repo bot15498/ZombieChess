151aee7b96a8e94655c1fb62832a9bc13ea5ac65
Ben Wang <ben135wang@gmail.com>
2024-07-06 03:18:40 -0400

    Add initial piece scripts

---
+++ ZombieChess/Assets/Scripts/Pieces/Queen.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static UnityEngine.UI.GridLayoutGroup;
+
+public class Queen : MonoBehaviour, IMoveablePiece
+{
+    [SerializeField]
+    private int _xPos = -1;
+    [SerializeField]
+    private int _yPos = -1;
+    [SerializeField]
+    private Board board;
+
+    public int xPos { get => _xPos; set => _xPos = value; }
+    public int yPos { get => _yPos; set => _yPos = value; }
+    public int health { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
+    public int maxHealth { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
+    public int numActions { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
+    public bool canAct { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
+    public CurrentTurn owner { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+
+    public bool Move(int newXPos, int newYPos)
+    {
+        // Return true if this is a valid move, other wise return false;
+        return false;
+    }
+
+    public bool Spawn(Board board, int xPos, int yPos, CurrentTurn owner)
+    {
+        this.board = board;
+        this.xPos = xPos;
+        this.yPos = yPos;
+        this.owner = owner;
+        return true;
+    }
+
+    public bool PreviewMove(out List<int> newXPos, out List<int> newYPos)
+    {
+
+        newXPos = new List<int> { xPos };
+        newYPos = new List<int> { yPos };
+
+        return true;
+    }
+}
